f64fd68c8ea75a1b90a49eb850db074fefd4398d
kurox <x@kuro-x.com>
2017-05-28 13:18:51 +0800

    Add account activation

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,9 @@ User.create!(name: "Example User",
              password:
                  "foobar",
              password_confirmation: "foobar",
-             admin: true)
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
 99.times do |n|
   name = Faker::Name.name
   email = "example-#{n + 1}@example.org"
@@ -19,5 +21,7 @@ User.create!(name: "Example User",
                email: email,
                password:
                    password,
-               password_confirmation: password)
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
 end
